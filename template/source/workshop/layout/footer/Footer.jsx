142c6634f117640cc51c976230449b5eb9a4a133
Michał Sobiecki <michalsobiecki@yahoo.com>
2017-11-08 17:04:26 +0100

    Wstepny store

--- template/source/workshop/layout/footer/Footer.jsx
+++ template/source/workshop/layout/footer/Footer.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 
+import * as FooterActions from "./FooterActions";
+import FooterStore from "./FooterStore";
+
 import styles from './Footer.sass';
 
 import FooterBrand from './footer-brand/Footer-brand';
@@ -10,6 +13,46 @@ import FooterCopyright from './footer-copyright/Footer-copyright';
 
 
 class Footer extends Component {
+	constructor() {
+		super();
+		this.state = {
+			loader: false,
+			footer: FooterStore.get()
+		}
+	};
+	componentDidMount() {
+		this.getFooter();
+	}
+
+	componentWillMount() {
+		FooterStore.on("fetch", this.setLoader.bind(this));
+		FooterStore.on("receive", this.setFooter.bind(this));
+	}
+
+	componentWillUnmount() {
+		FooterStore.removeListener("fetch", this.setLoader.bind(this));
+		FooterStore.removeListener("receive", this.setFooter.bind(this));
+	}
+
+	getFooter() {
+		FooterActions.getFooter();
+	}
+
+	setFooter() {
+		console.log('[Footer] Uzyskanie danych')
+		this.setState({
+			footer: FooterStore.get(),
+			loader: false
+		});
+	}
+
+	setLoader() {
+		console.log('[Footer] Ustawienie loadera')
+		this.setState({
+			loader: true
+		});
+	}
+
 	render() {
 		return (
 			<footer class="footer">
@@ -39,9 +82,5 @@ class Footer extends Component {
 		);
 	}
 }
-
-Footer.defaultProps = {
-
-};
 
 export default Footer;
