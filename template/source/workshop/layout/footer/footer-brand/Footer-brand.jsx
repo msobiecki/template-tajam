b6038656d350e5697a47093e8f187c32975e7100
Michał Sobiecki <michalsobiecki@yahoo.com>
2017-11-19 16:16:35 +0100

    Save-point

--- template/source/workshop/layout/footer/footer-brand/Footer-brand.jsx
+++ template/source/workshop/layout/footer/footer-brand/Footer-brand.jsx
@@ -11,7 +11,7 @@ function FooterBrand() {
         <img src={FooterBrandImage} alt="Tajm" />
       </div>
       <div className="footer-brand__text">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce maximus nisl at neque dictum mollis. Donec nisi magna, molestie imperdiet viverra non, porta in erat. Etiam ultricies lobortis diam, nec scelerisque eros ornare non. Suspendisse leo ex, bibendum et nunc in, dignissim pellentesque nisi. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam iaculis imperdiet nibh. Quisque a ultrices risus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Cras eget nisi vitae nulla tempus vulputate sit amet maximus enim. Etiam in dolor quis neque rhoncus tempus vitae quis erat.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce maximus nisl at neque dictum mollis. Donec nisi magna, molestie imperdiet viverra non, porta in erat. Etiam ultricies lobortis diam, nec scelerisque eros ornare non.
       </div>
     </div>
   );
